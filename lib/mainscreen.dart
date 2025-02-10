db274cf643897801a91fc07ad91026625946a745
Kimjongwon1 <amricanoblack@naver.com>
2025-02-10 10:08:05 +0900

    first_commit

---
+++ lib/mainscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Mainscreen extends StatefulWidget {
+  const Mainscreen({super.key});
+
+  @override
+  State<Mainscreen> createState() => _State();
+}
+
+class _State extends State<Mainscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
